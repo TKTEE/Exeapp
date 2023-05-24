799f8eaab81eeebe92b9a057be2584834b468e5c
eMobilis <consolatamburu99@gmail.com>
2023-05-24 16:03:05 +0300

    exe app

--- app/src/main/java/com/example/exeapp/WebsiteActivity.kt
+++ app/src/main/java/com/example/exeapp/WebsiteActivity.kt
@@ -2,10 +2,16 @@ package com.example.exeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class WebsiteActivity : AppCompatActivity() {
+    lateinit var myWebView:WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_website)
+        myWebView = findViewById(R.id.mWeb)
+        var settings = myWebView.settings
+        settings.javaScriptEnabled = true
+        myWebView.loadUrl("https://www.emobilis.ac.ke")
     }
 }
